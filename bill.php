792dc188be3f2b8a321e30cf2f0509876ef1178d
harsh9090 <harsh.mithaiwala.hm@gmail.com>
2019-07-01 17:15:22 +0530

    hotel management

---
+++ bill.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta http-equiv="Content-Security-Policy"
+        content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; img-src 'self' data: content:;">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="viewport"
+        content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+    <!--<link rel="stylesheet" type="text/css" href="css/index.css">-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <title>welcome to our hotel page</title>
+    <style type="text/css">
+		*{font-family: "Helvetica"}
+    .zoom {
+  transition: transform .2s; /* Animation */
+  margin: 0 auto;
+}
+
+.zoom:hover {
+  transform: scale(1.5); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+}
+  		div{
+			border: 0px black solid;
+			position: absolute;
+		}
+		a{
+	text-decoration: none;
+}
+	.p1 {
+  	padding-left: 175px;
+  	
+}
+.font2{
+color:white;
+}
+ .font1{
+color:white;
+ 
+ }
+ .img1 {
+  -webkit-filter: blur(4px); /* Safari 6.0 - 9.0 */
+  filter: blur(4px);
+}
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+}
+}
+
+		
+	</style>
+</head>
+
+<body>
+    <b><font size="7" class="font2"><p class="p1">Hotel Taj</font></b></p>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top">
+     
+        
+        <a href="index.html"><button type="button" class="btn btn-outline-primary">HOME</button></a>
+        <a href="admin.html"><button type="button" class="btn btn-outline-dark">ADMIN</button> </a>
+        <a href="customer.html"><button type="button" class="btn btn-outline-success">CUSTOMERS DETAILS</button></a>
+        <a href="serve.html"><button type="button" class="btn btn-outline-info">CO-WORKERS</button></a>
+        <a href="room.html"> <button type="button" class="btn btn-outline-danger">ROOMS</button></a>
+        <a href="bill.html"> <button type="button" class="btn btn-outline-dark">Bill</button></a>
+            </nav>
+            <div class="container">
+                                <?php
+                                  ini_set('mysql.connection_timeout',300);
+                                  ini_set('default_socket_timeout',300);
+
+                                  $servername = "localhost:3306";
+                                  $username = "root";
+                                  $password = "";
+                                  $dbname = "project";
+
+                                  // Create connection
+                                  $conn = new mysqli($servername, $username, $password, $dbname);
+                                  // Check connection
+                                  if ($conn->connect_error) {
+                                      die("Connection failed: " . $conn->connect_error);
+                                  } 
+
+                                  $sql = "SELECT * FROM bill";
+                                  $result = $conn->query($sql);
+
+                                  if ($result->num_rows > 0) {
+                                      echo "<center class='text-info'><table class='table table1 table-striped'><thead><tr><th>c_id</th><th>room_no</th><th>type</th><th>cost</th><th>type</th><th>cost</th></tr></thead>";
+                                      // output data of each row
+                                      while($row = $result->fetch_assoc()) {
+                                          echo "<tr><td>".$row["type"]."</td><td>".$row["cost"]."</td></tr>";
+                                      }
+                                      echo "</table></center>";
+                                  } else {
+                                      echo "0 results";
+                                  }
+                                  $conn->close();
+                                  ?>
+                     
+            </div>
+</body>
+</html>
